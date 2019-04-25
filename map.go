e7f7447469e13b94a901564a99fead9d50da8167
bo <40255498+cnliboo@users.noreply.github.com>
2019-04-25 17:57:53 +0800

    Create map.go

---
+++ map.go
@@ -0,0 +1,15 @@
+// map is disordered
+// It's also a reference type
+map1 := make(map[string]int)
+map1["first"] = 1
+
+// make
+// returns a non-zero value
+// for map, slice, channel initialization
+
+// new
+// returns a pointer *T
+new(T)
+
+// delete
+delete(map1, "first")
